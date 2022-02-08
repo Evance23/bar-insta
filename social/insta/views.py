7f1bd6179c846b394e539b0c318d5cde94a00f4a
Evance23 <evancebarracks01@gmail.com>
2022-02-08 03:35:37 +0300

    Adding new templates and base files

--- social/insta/views.py
+++ social/insta/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from django.views import Views 
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Insta')
-# Create your views here.
+
+
+
+class Index(View):
+    def get(self, request, 'args', 'kwargs'):
+        return render(render, landing/index.html)
